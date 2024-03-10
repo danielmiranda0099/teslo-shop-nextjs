f6433f9665943d298ecae4004a8e281a26b1d4ac
danielmiranda0099 <danielmesa877@gmail.com>
2024-03-10 13:40:03 -0500

    [ADD] Navbar Menu Component

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,3 +3,6 @@ export { Title } from "./title/Title";
 export { PageNotFound } from "./not-found/PageNotFound";
 
 export { TopMenu } from "./top-menu/TopMenu";
+
+export { SidebarMenu } from "./sidebar-menu/SidebarMenu";
+
