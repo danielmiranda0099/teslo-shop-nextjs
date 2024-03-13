ccb13452f3d24eb73abf95f5868424d63603ac81
danielmiranda0099 <danielmesa877@gmail.com>
2024-03-13 17:35:42 -0500

    [ADD IU] Purchase Checkout Screen

---
+++ src/app/(shop)/checkout/page.tsx
@@ -0,0 +1,104 @@
+import { QuantitySelector } from "@/components/products";
+import { Title } from "@/components/ui";
+import { initialData } from "@/seed/seed";
+import Image from "next/image";
+import Link from "next/link";
+
+const PRODUCTS_IN_CART = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2],
+];
+
+export default function CheckoutPage() {
+  return (
+    <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+      <div className="flex flex-col w-[1000px]">
+        <Title title="Verificar Orden" />
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+          <div className="flex flex-col mt-5">
+            <span className="text-xl">Ajustar Elementos</span>
+            <Link href="/cart" className="underline mb-5">
+              Editar Carrito
+            </Link>
+
+            {PRODUCTS_IN_CART.map((product) => (
+              <div key={product.slug} className="flex mb-5">
+                <Image
+                  src={`/products/${product.images[0]}`}
+                  alt={product.title}
+                  width={100}
+                  height={100}
+                  className="mr-5 rounded"
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                  }}
+                />
+
+                <div>
+                  <p>{product.title}</p>
+                  <p>${product.price} x 3</p>
+
+                  <p className="font-bold">Subtotal: ${product.price * 3}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className="bg-white rounded-xl shadow-xl p-7 h-fit">
+            <h2 className="text-2xl mb-2">Dirección De Entrega</h2>
+            <div className="mb-10">
+              <p>Danie Miranda</p>
+              <p>Urb Los Privilegios</p>
+              <p>Magdalena</p>
+              <p>Santa Marta</p>
+              <p>CP 47000</p>
+              <p>3008891708</p>
+            </div>
+
+            <div className="w-full h-0.5 rounded bg-gray-200 mb-10"></div>
+
+            <h2 className="text-2xl mb-2">Resumen De Orden</h2>
+
+            <div className="grid grid-cols-2">
+              <span>No. Productos</span>
+              <span className="text-right">3 Artículos</span>
+
+              <span>Subtotal</span>
+              <span className="text-right">$ 100</span>
+
+              <span>Impuestos (15%)</span>
+              <span className="text-right">$ 100</span>
+
+              <span className="mt-5 text-2xl">Total:</span>
+              <span className="mt-5 text-2xl text-right">$ 100</span>
+            </div>
+
+            <div className="mt-5 mb-2 w-full">
+              <div>
+                <p className="mb-5">
+                  <span className="text-xs">
+                    Al Hacer Click En "Hacer Orden", Aceptas nuestros{" "}
+                    <Link href="/" className="underline">
+                      Terminos Y Condiciones
+                    </Link>
+                    Y
+                    <Link href="/" className="underline">
+                      Politicas De Privacidad
+                    </Link>
+                  </span>
+                </p>
+              </div>
+
+              <Link href="/orders/123" className="flex btn-primary">
+                Hacer Orden
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
