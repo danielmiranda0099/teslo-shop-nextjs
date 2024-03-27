e604317fcbca7a4e28eb762290601888f783497c
danielmiranda0099 <danielmesa877@gmail.com>
2024-03-26 21:30:05 -0500

    [UP UI] Skeleton StockLabel

--- src/components/products/StockLabel.tsx
+++ src/components/products/StockLabel.tsx
@@ -9,19 +9,31 @@ interface Props {
 
 export function StockLabel({ slug }: Props) {
   const [stock, setStock] = useState(0);
+  const [isLoading, setIsLoading] = useState(true)
 
   const GetStock = async () => {
     const stock = await GetStockBySlug(slug);
     setStock(stock);
+    setIsLoading(false);
   }
 
   useEffect(() => {
     GetStock();
   }, [])
 
+  if(isLoading) return <Skeleton />
+
   return (
     <h1 className={`${titleFont.className} antialiased font-bold text-md`}>
       Stock: { stock }
+    </h1>
+  )
+}
+
+function Skeleton() {
+  return(
+    <h1 className={`${titleFont.className} antialiased font-bold text-md bg-gray-200 animate-pulse`}>
+      &nbsp;
     </h1>
   )
 }
